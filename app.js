404b6383f467da2cedd90aac5a3bc3adffad00b4
Amir Salehi <nik.amirsa@gmail.com>
2022-06-07 18:57:10 +0430

    handle test routes

--- app.js
+++ app.js
@@ -3,14 +3,28 @@ import express from "express";
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 // clear the console on every request
 app.use((req, res, next) => {
     console.clear();
-    console.log('hi');
     next();
 });
+
+app.route('/test')
+    .get((req, res) => {
+        res.send('<h1>this is get req to main page</h1>');
+        console.log(req.body)
+    })
+    .post((req, res) => {
+
+        console.log(req.body)
+        res.status(400)
+
+        res.send({
+            id: 'test'
+        })
+    })
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000 http://localhost:3000");
